c048051f87330ac619f7bf2b2b312deddb1243f4
XxRogeoxX <ronaldsan3.3@gmail.com>
2026-04-12 18:28:26 -0500

    Fead : Add function moverIzquierda and moverDerecha

--- limones.js
+++ limones.js
@@ -4,18 +4,38 @@ let ctx=canvas.getContext("2d");
 const ALTURA_SUELO=40;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
+let personajeX=canvas.width/2;
 
 function dibujarSuelo(){
-    ctx.fillStyle="blues"
+    ctx.fillStyle="black"
     ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width,ALTURA_SUELO);
 }
 
 function dibujarPersonaje(){
     ctx.fillStyle="green"
-    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
 
 }
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
+}
+
+function moverIzquierda(){
+    personajeX=personajeX-10;
+    actualizarPantalla();
+}
+function moverDerecha(){
+    personajeX=personajeX+10;
+    actualizarPantalla();
+}
+
+function actualizarPantalla(){
+    limpiarCanvas();
+    dibujarSuelo();
+    dibujarPersonaje();
+
+}
+function limpiarCanvas(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
 }
